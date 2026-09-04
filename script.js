fbf2f77849a8b81ca91c9baf14c6d5b91e96767a
marjorie091505@gmail.com <marjorie091505gmail.com>
2026-09-04 14:39:42 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 console.log("hello world");
 console.log("from script.js file");
+const myName = "Marjorie";
+let age = 21;
+let number = "09876543211";
+let address = "brgy 19-A canetown subdivision"
+let saiseyte = "67"
+
+
+
+console.log(`My name is ${myName}`);
+console.log(`age: ${age}`);
+console.log(`number: ${number}`);
+console.log(`address: ${address}`);
+console.log(67);
+
