794a2fc8290fc63e9b985ce8c6c6040f7d56b11a
marjorie091505@gmail.com <marjorie091505gmail.com>
2026-09-04 17:30:17 +0800

    addEventListener

--- script.js
+++ script.js
@@ -26,15 +26,37 @@ console.log(greet("mayung aga" , ["guian", "jere", "dy", "marjorie"] ));
 
 const heading =  document.querySelector("h1");
 heading.textContent = "Joeffer Gonzaga"
-heading.style.color = "pink";
-heading.style.backgroundColor = "black";
+heading.style.color = "purple";
+heading.style.backgroundColor = "yellow";
 heading.style.fontSize = "50px";
-console.log(heading); 
+console.log(heading);
+
+heading.addEventListener("click", function(){
+heading.style.color = "red";
+console.log("Heading was clicked.");
+})
 
  const contactHeading = document.querySelector("#contact form");
  console.log(contactHeading);
 
  const projectHeading = document.querySelector("#services h2");
  console.log(projectHeading);
+
+
+ const toggleButton = document.querySelector('#switch');
+ const body = document.querySelector('body');
+ let isOff  = false ;
+
+ toggleButton.addEventListener("click", function() {
+isOff = !isOff;
+    if (isOff) {
+        body.style.background = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white"
+        body.style.color = "black";
+    }
+ })
+
 
 
